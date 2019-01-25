3459f1556c9352d849bd88b1e652e5013a7830b2
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-25 17:10:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,9 @@ require 'pry'
 class CashRegister
 attr_accessor :total, :price,:title, :discount
 
-  def initialize(total = 0)
-    @total =total
-    @discount = total
+  def initialize(discount = 0)
+    @total =0
+    @discount = 
   end
 
   def self.add_item(title,price)
