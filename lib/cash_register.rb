11ec972bdd893b95fb047e9ce7e71acd3076f007
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-25 17:41:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class CashRegister
 attr_accessor :total, :discount
+attr_reader :item
 
   def initialize(discount = 0)
     @total =0
@@ -12,9 +13,9 @@ attr_accessor :total, :discount
   end
 
     def apply_discount
-      if dicount !=0
-      @total-= total*discount/100
-      "After the discount, the total comes to #{total}"
+      if discount !=0
+      @total-=  total*discount/100
+      "After the discount, the total comes to #{total}."
       else
       "There is no discount to apply"
       end
