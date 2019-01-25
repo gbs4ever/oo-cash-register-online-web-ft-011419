2b758910cc303848460bf0923679c2bca75e16e7
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-25 17:49:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ attr_reader :item, :discount
 
   def add_item(title, price, quantity=1)
     @total += price * quantity
-    
+
   end
 
     def apply_discount
@@ -27,7 +27,7 @@ attr_reader :item, :discount
     end
 
     def void_last_transaction
-      
+
     end
 
 
